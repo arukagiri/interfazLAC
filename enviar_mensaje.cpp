085fc0f0ff85ee0f89f211c040500a2a22248f05
Lucas Sandoz <sandozlucas1995@gmail.com>
2019-02-20 18:52:43 -0300

    Before merge

--- enviar_mensaje.cpp
+++ enviar_mensaje.cpp
@@ -41,6 +41,7 @@ Enviar_Mensaje::Enviar_Mensaje(QWidget *parent) :
     ui->list_DESTINO->addItem("Volante de Inercia",QVariant(LACAN_ID_VOLANTE));
     ui->list_DESTINO->addItem("Boost",QVariant(LACAN_ID_BOOST));
 
+    ui->spin_valor->setMaximum(1000);
 
   //  ui->list_DESTINO->setCurrentIndex(mw->dest);
     switch(mw->dest){
